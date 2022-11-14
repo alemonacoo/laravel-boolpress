59c33de8443fdf2650db0e87226bcee03a0ab475
alemonacoo <ale.monaco@outlook.com>
2022-11-14 23:52:34 +0100

    Adding show

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row">
         @foreach ($posts as $post)
-            <p>{{$post['title']}}</p>
+            <a href="{{route('admin.posts.show', $post->id)}}"><p>{{$post['title']}}</p></a>
 
             
         @endforeach
---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.dashboard')
+
+@section('content')
+     <h5>{{$post->title}}</h3>
+     <p>{{$post->content}}</p>
+
+        
+@endsection
