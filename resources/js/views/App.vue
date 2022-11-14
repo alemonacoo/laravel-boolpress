5e07d5d4bbad79e4ee093a2d21daf76c236998af
alemonacoo <ale.monaco@outlook.com>
2022-11-14 22:23:50 +0100

    Adding guest home + vue connection

---
+++ resources/js/views/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>div di prova</div>
+</template>
+
+<script>
+export default {
+    name: "App",
+};
+</script>
+
+<style scoped lang="scss"></style>
--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- csfr token --}}
+    <meta name="csrf_token" content="{{csrf_token()}}">
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+    <script src="{{ asset('js/front.js')}}" defer></script>
+
     <title>{{ config('app.name', 'Boolpress') }}</title>
 </head>
 <body> 
-    
+    <div id="root">
+        ciao
+    </div>
 </body>
 </html>
