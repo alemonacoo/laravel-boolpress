12e004900a8f97b804795a9c508115b241f9e0b4
alemonacoo <ale.monaco@outlook.com>
2022-11-15 16:25:40 +0100

    Excercise completed

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,13 +5,11 @@
     @csrf
     <div>
         <label for="title">Titolo:</label>
-        <input type="text" required minlength="1" maxlength="255" name="title" value="{{old('title', ' ')}}">
+        <input type="text" required minlength="5" maxlength="255" name="title" value="{{old('title', ' ')}}">
     </div>
     <div>
         <label for="content">Contenuto:</label>
-        <textarea name="content" maxlength="255" cols="30" rows="10" value="{{old('content', ' ')}}"></textarea>
+        <textarea name="content" required maxlength="255" cols="30" rows="10" value="{{old('content', ' ')}}"></textarea>
     </div>
-
-    <input type="submit" value="Crea">
     </form>
 @endsection
