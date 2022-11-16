1a2324c5d4fc4773d1b859084ca77bfba8d3a8be
alemonacoo <ale.monaco@outlook.com>
2022-11-17 00:02:37 +0100

    adding seeder

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+use Illuminate\Support\Str;
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $categories = ['Film', 'Leaks', 'Interviste', 'Curiosità', 'Eventi', 'Altro', 'Notizie'];
+
+        foreach($categories as $model){
+            $category = new Category();
+            $category->name = $model;
+            $category->slug = Str::slug($category->name);
+            $category->save();
+        }
+    }
+}
