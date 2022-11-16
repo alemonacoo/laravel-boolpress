4cbf7109cf6e27b2396c419486b000b1de194f83
alemonacoo <ale.monaco@outlook.com>
2022-11-17 00:46:09 +0100

    Adding category in show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,6 +3,11 @@
 @section('content')
      <h5>{{$post->title}}</h3>
      <p>{{$post->content}}</p>
+     @if ($post->category)
+          <p>CATEGORIA: {{ $post->category->name }}</p>
+     @else
+          <p>Nessuna Categoria</p>
+     @endif
      <button>
      <a href="{{route('admin.posts.edit', $post)}}">Modifica</a>
      </button>
