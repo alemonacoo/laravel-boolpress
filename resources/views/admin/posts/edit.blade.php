7e6251bf50665daa0527d7a5823f0bf2adc2ae2a
alemonacoo <ale.monaco@outlook.com>
2022-11-17 00:52:58 +0100

    Adding categories in post edit

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -12,6 +12,17 @@
         <label for="content">Contenuto:</label>
         <textarea name="content" cols="50" rows="10">{{old('content', $post->content)}}</textarea>
     </div>
+    <div>
+        <label for="category_id">Categoria:</label>
+        <select name="category_id">
+            <option value="">Nessuna</option>
+            @foreach ($categories as $category)
+            <option value="{{ $category->id }} {{$category->id == old('category_id', $post->category_id) ? 'selected' : ''}}">
+                {{ $category->name }}
+            </option>    
+            @endforeach
+        </select>
+        </div>
     <input type="submit" value="Aggiorna">
     </form>
 @endsection
