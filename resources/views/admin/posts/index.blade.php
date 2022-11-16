5fef67df2138d2d0373485a884397e5dd57d8783
alemonacoo <ale.monaco@outlook.com>
2022-11-17 00:57:12 +0100

    adding categories in posts index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -8,6 +8,9 @@
         @foreach ($posts as $post)
             <p>
             <a href="{{route('admin.posts.show', $post->id)}}">{{$post['title']}}</a>
+             @if ($post->category)
+                [{{$post->category->name}}]
+             @endif
             </p>
         @endforeach
 
