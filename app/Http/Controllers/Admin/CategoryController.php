e03a3a0a1ad9e9a32caad606cf38c1de4970ae29
alemonacoo <ale.monaco@outlook.com>
2022-11-21 22:07:29 +0100

    Adding delete for categories

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -100,9 +100,11 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
         //
+        $category->delete();
+        return redirect()->route('admin.categories.index');
     }
 
     //TODO: Should move in trait
--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -2,8 +2,14 @@
 
 @section('content')
     <h4>{{$category->name}}</h4>
-    <button><a href="{{ route('admin.categories.edit', $category->id) }}">Edit</a></button>
-    {{-- <button><a href="{{ route('admin.categories.edit') }}">ELIMINA</a></button> --}}
+    <button class="my-2"><a href="{{ route('admin.categories.edit', $category->id) }}">Edit</a></button>
+    <form action="{{ route('admin.categories.destroy', $category->id) }}" method="POST">
+    @csrf
+    @method('DELETE')
+    <button class="my-2" type="submit" value="delete">Delete</button>
+    </form>
+
+
     @foreach($category->posts as $post)   
     <div class="my-2">
     <a href="{{route('admin.posts.show', $post->id)}}">{{$post['title']}}</a>
