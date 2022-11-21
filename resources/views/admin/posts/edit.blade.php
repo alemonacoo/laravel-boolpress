0d1967a7a34e873a636443970a9219e35e1700a3
alemonacoo <ale.monaco@outlook.com>
2022-11-21 15:56:06 +0100

    adding tags in edit

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -22,7 +22,32 @@
             </option>    
             @endforeach
         </select>
+    </div>
+
+        {{-- Tags: --}}
+    @if($errors->any())
+        <div>
+        <label>Tags:</label>
+        @foreach ($tags as $tag)
+            <input 
+            {{ in_array($tag->id, old('tags', [])) ? 'checked' : ''}} 
+            type="checkbox" name="tags[]" value="{{$tag->id}}">
+        <label>{{ $tag['name']}}</label>
+
+        @endforeach
         </div>
+    @else
+        <div>
+        <label>Tags:</label>
+        @foreach ($tags as $tag)
+            <input 
+            {{ $post->tags->contains($tag) ? 'checked' : ''}} 
+            type="checkbox" name="tags[]" value="{{$tag->id}}">
+        <label>{{ $tag['name']}}</label>
+
+        @endforeach
+        </div>
+    @endif
     <input type="submit" value="Aggiorna">
     </form>
 @endsection
