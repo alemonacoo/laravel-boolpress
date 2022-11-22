fd009fac747ec271c7f20d75f552688e8cc07f29
alemonacoo <ale.monaco@outlook.com>
2022-11-22 20:59:48 +0100

    Adding cover_path in fillable

--- app/Post.php
+++ app/Post.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
-    protected $fillable = ['title', 'content', 'slug', 'category_id'];
+    protected $fillable = ['title', 'content', 'slug', 'category_id', 'cover_path'];
 
     public function category(){
         return $this->belongsTo('App\Category');
-    }
+    } 
 
     public function tags(){
         return $this->belongsToMany('App\Tag');
